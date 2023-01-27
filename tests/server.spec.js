bc889caf99fc7bec13c8f3fc2b520f9460899d23
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2023-01-27 12:46:38 -0300

    feat: A function is created to detect a maximum id, which is obtained by searching for the maximum id of the cafes.json file and adding 1. This for two functions in which a non-existent id is required

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,7 @@
-const request = require("supertest");
-const server = require("../index");
+const request = require('supertest')
+const server = require('../index')
+const cafes = require('../cafes.json')
+const fs = require('fs')
+const kill = require('kill-port')
 
-describe("Operaciones CRUD de cafes", () => {
-
-});
+describe('Operaciones CRUD de cafes', () => {})
